9ead8368eb3ab38a0756652518e75119baf0f524
Pavel <pashak17@ukr.net>
2016-03-04 01:20:30 +0200

    add console command

---
+++ reload.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+APACHEUSER=`ps aux | grep -E '[a]pache|[h]ttpd|[_]www|[w]ww-data' | grep -v root | head -1 | cut -d\  -f1`
+
+setfacl -R -m u:"$APACHEUSER":rwX -m u:`whoami`:rwX app/cache app/logs web/
+setfacl -dR -m u:"$APACHEUSER":rwX -m u:`whoami`:rwX app/cache app/logs web/
+
+
+#echo "$1 parameters";
+
+action="$1"
+
+if [ -z "$1" ]; then
+    action="-help"
+fi
+
+install_run()
+{
+    composer install
+    php bin/console doctrine:database:create
+    php bin/console doctrine:schema:update --force
+    php bin/console cache:clear
+    php bin/console cache:clear -e prod
+    npm install
+    ./node_modules/.bin/bower install --allow-root
+    ./node_modules/.bin/gulp
+}
+
+
+update_run ()
+{
+    composer install
+    php bin/console doctrine:database:drop --force
+    php bin/console doctrine:database:create
+    php bin/console doctrine:schema:update --force
+    php bin/console cache:clear
+    php bin/console cache:clear -e prod
+    npm install
+    ./node_modules/.bin/bower install --allow-root
+    ./node_modules/.bin/gulp
+}
+
+case "$action" in
+    -install)
+        install_run
+        ;;
+    -update)
+        update_run
+        ;;
+    -help)
+        echo "Available options"
+        echo "-install : Install project"
+        echo "-update : Update project"
+        ;;
+    *)
+        echo "undefined action"
+        ;;
+esac
